d23d905db8856d948dfe26e3e4c6bdf900355d9f
frederico nakajima <fredericonakajimanakajima@gmail.com>
2026-05-08 15:01:00 -0300

    coloquei link do frontend no netlify

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://nakak.netlify.app"
+        "https://capable-sopapilas-29c9fa.netlify.app"
     ],
     credentials: true
 }));
